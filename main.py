8c6f71a10275dc9e8608e107fd3c4f38ac9926ad
Кирилл <kirill.kobelkov2007@gmail.com>
2021-12-13 16:53:10 +0300

    com2

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 import sys
 
-from PyQt5 import uic
 from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtWidgets import QWidget, QApplication, QPushButton, QMainWindow
-from random import randrange
+from random import randrange, randint
 
 
-class MyWidget(QMainWindow):
+class MyWidget(QWidget):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.initUI()
+        self.do_paint = False
+        self.btn.clicked.connect(self.paint)
 
+    def initUI(self):
         self.setGeometry(300, 300, 600, 600)
         self.setWindowTitle('Круги')
+        self.btn = QPushButton('Нажмите', self)
+        self.btn.resize(100, 100)
+        self.btn.move(275, 325)
         self.do_paint = False
         self.btn.clicked.connect(self.paint)
 
@@ -29,10 +34,10 @@ class MyWidget(QMainWindow):
 
     def draw_flag(self, qp):
         n = randrange(150)
-        qp.setBrush(QColor(255, 255, 0))
+        qp.setBrush(QColor(*[randint(0, 255) for i in range(3)]))
         qp.drawEllipse(60, 100, n, n)
         nu = randrange(150)
-        qp.setBrush(QColor(255, 255, 0))
+        qp.setBrush(QColor(*[randint(0, 255) for i in range(3)]))
         qp.drawEllipse(300, 100, nu, nu)
 
 
